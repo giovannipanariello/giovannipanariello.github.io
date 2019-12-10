46c4713643410206241ff834fd8b10eb36b34f3e
Giovanni Panariello <giovanni.panariello@intarget.net>
2019-12-10 10:50:10 +0100

    Switch to PHP files

--- about.php
+++ about.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>GSC</title>
-		<script type="text/javascript" src="js/scripts.js"></script>
-		<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">		
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<link rel="manifest" href="/manifest.json">
-	</head> 
-
-	<body>
-		<nav class="nav">
-			<a class="nav__link" href="/">Home</a>
-			<a class="nav__link" href="about.html">About</a>
-		</nav>
+<?php require("header.php") ?>
 
 	<div class="container">
 		<h1>About</h1>
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php require("header.php") ?>
+	
+		<div class="container">
+			<h1>Shop</h1>
+			<div class="row">
+				<div class="col-md-8">
+					<div class="box">
+						
+						<img src="img/upmann.png">
+						<h2>Upmamm</h2>
+
+					</div>					
+				</div>	
+				<div class="col-md-4">
+
+					<div class="box box__small">
+				
+						<img src="img/toscani.jpg">
+						<h2>Toscani</h2> 
+
+					</div>
+					<div class="box box__small"> 
+						
+						<img src="img/whisky.jpg">
+						<h2>Buono!</h2>
+
+					</div>
+				</div>
+			
+			</div>
+		</div>
+
+		<!-- The core Firebase JS SDK is always required and must be listed first -->
+		<!-- <script src="/__/firebase/7.5.0/firebase-app.js"></script> -->
+
+		<!-- TODO: Add SDKs for Firebase products that you want to use
+		     https://firebase.google.com/docs/web/setup#available-libraries -->
+		<!-- <script src="/__/firebase/7.5.0/firebase-analytics.js"></script> -->
+
+		<!-- Initialize Firebase -->
+		<!-- <script src="/__/firebase/init.js"></script> -->
+
+		<!-- <script type="text/javascript" src="node-server.js"></script> -->
+	</body>
+</html>
